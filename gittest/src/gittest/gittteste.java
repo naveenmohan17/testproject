061edb808a229982c2d5f6512555ce68f8663f6a
naveenmohan17 <mnaveen185q@gmail.com>
2019-12-06 11:02:59 +0530

    commit 4

--- gittest/src/gittest/gittteste.java
+++ gittest/src/gittest/gittteste.java
@@ -5,7 +5,7 @@ public class gittteste {
 	public static void main(String[] args) {
 		System.out.println("commit 1");
 		System.out.println("commit 2");
-		System.out.println("commit 3");
+		
 	}
 
 }
