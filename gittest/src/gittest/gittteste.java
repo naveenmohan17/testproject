50bfb0b8782fbd32cbcc9a05cc74d3e0f95d5124
naveenmohan17 <mnaveen185q@gmail.com>
2019-12-09 12:18:27 +0530

    Reverse a string

--- gittest/src/gittest/gittteste.java
+++ gittest/src/gittest/gittteste.java
@@ -1,11 +1,30 @@
 package gittest;
 
-public class gittteste {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		System.out.println("commit 1");
-		System.out.println("commit 2");
-		
+public class gittteste {
+	public static void main(String args[]) {
+	String[] inputList = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+			"aug", "Sep", "Oct", "nov", "Dec" };
+
+	// Display input un-sorted list.
+	System.out.println("-------Input List-------");
+	showList(inputList);
+
+	// Call to sort the input list.
+	Arrays.sort(inputList);
+
+	// Display the sorted list.
+	System.out.println("\n-------Sorted List-------");
+	showList(inputList);
+
 	}
 
+	private static void showList(String[] array) {
+		// TODO Auto-generated method stub
+		for (String str : array) {
+			System.out.print(str + " ");
+		
+	}	
+}
 }
